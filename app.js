23b33a0bbe644aaf0cdedebe16c56e4bda3dffc0
Ali mehdi <110098560+alimehdi2001@users.noreply.github.com>
2024-03-02 21:55:35 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
-// 1. Write a script to greet your website visitor using JS alert box.
-alert("Assamualaikum")
+// ------------------ Assignment#2------------------
 
-// 2. Write a script to display following message on your web page .
- alert("Error! please enter a valid password.")
+// 1. Declare a variable called username.
+var username= 'Ali';
 
-//  3. Write a script to display following message on your web page: (Hint : Use line break)
-alert("Welcome to JS land \n happy coding!" )
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+ var myName= "ALI MEHDI";
 
-// 4. Write a script to display following messages in sequence:
-alert("Welcome to JS land")
-alert("Happy coding!")
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+var message = "Hello World";
+alert(message);
 
-// 5. Generate the following message through browser’s developer console:
-console.log("Hello... I can run JS through my web browser's console");
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var studentname = "Johone Doe";
+var age = "15 years old";
+var trainer = "certified Mobile Application Development"
+
+alert(studentname)
+alert(age)
+alert(trainer)
+
+// 5. Write a script to display the following alert using one JS variable:
+var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation).
+var email = "alimehdi72@gmail.com"
+alert ( "your email is : " + email);
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+ var book = "A smarter way to learn JavaScript";
+ alert(book);
+
+//  8. Write a script to display this in browser through JS.
+var browser = "YAh! I can write HTML content through Javascript "  ;
+document.write(browser) ; 
+ "<br/>"
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+var symbol = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+alert(symbol);
+document.write(symbol);
